a0bd2e6f3d2f1b438e9b401834c0975044cff520
kidow <wcgo2ling@hanmail.net>
2018-11-26 02:30:59 +0900

    containers login onChange set

--- client/src/containers/Auth/Login.js
+++ client/src/containers/Auth/Login.js
@@ -3,13 +3,36 @@ import AuthContent from 'components/Auth/AuthContent';
 import InputWithLabel from 'components/Auth/InputWithLabel';
 import AuthButton from 'components/Auth/AuthButton'
 import RightAlignedLink from 'components/Auth/RightAlignedLink'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as authActions from '../../store/auth'
 
 class Login extends Component {
+  handleChange = e => {
+    const { AuthActions } = this.props
+    const { name, value } = e.target
+    AuthActions.changeInput({name, value, form: 'login'})
+  }
   render() {
+    const { email, password } = this.props.form.toJS()
+    const { handleChange } = this
     return (
       <AuthContent title='로그인'>
-        <InputWithLabel label='이메일' name='email' placeholder='이메일'/>
-        <InputWithLabel label='비밀번호' name='password' placeholder='비밀번호' type='password'/>
+        <InputWithLabel 
+          label='이메일' 
+          name='email' 
+          placeholder='이메일'
+          value={email}
+          onChange={handleChange}
+        />
+        <InputWithLabel 
+          label='비밀번호' 
+          name='password' 
+          placeholder='비밀번호' 
+          type='password'
+          value={password}
+          onChange={handleChange}
+        />
         <AuthButton>로그인</AuthButton>
         <RightAlignedLink to='/auth/register'>회원가입</RightAlignedLink>
       </AuthContent>
@@ -17,4 +40,11 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default connect(
+  state => ({
+    form: state.auth.getIn(['login', 'form'])
+  }),
+  dispatch => ({
+    AuthActions: bindActionCreators(authActions, dispatch)
+  })
+)(Login);
