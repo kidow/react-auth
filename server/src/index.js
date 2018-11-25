ac30b07529de40dacf529a03473fb50d0944835f
kidow <wcgo2ling@hanmail.net>
2018-11-25 19:53:43 +0900

    mongoose connect

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,24 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const port = process.env.PORT || 4000
 
 const apiRouter = require('./api')
 
 const app = express()
+
+mongoose.Promise = Promise
+mongoose.connect(process.env.MONGO_URI, {
+  useCreateIndex: true,
+  dbName: 'reactauth',
+  useNewUrlParser: true
+}, err => {
+  if (err) {
+    console.log('mongodb error :', err)
+  } else {
+    console.log('mongodb connected.')
+  }
+})
 
 app.use('/api', apiRouter)
 
