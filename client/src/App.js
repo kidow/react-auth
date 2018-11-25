c6b9f4882b20d9d4dd1bd925a7be77772849b987
kidow <wcgo2ling@hanmail.net>
2018-11-26 01:09:53 +0900

    header set

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import { HomePage, AuthPage } from './pages';
+import HeaderContainer from './containers/Base/HeaderContainer';
 
 class App extends Component {
   render() {
     return (
       <Switch>
+        <HeaderContainer />
         <Route exact path='/' component={HomePage}/>
         <Route path='/auth' component={AuthPage}/>
       </Switch>
