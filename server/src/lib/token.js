9cd5ba318eae557088dc6c6709ba2117c7d56190
kidow <wcgo2ling@hanmail.net>
2018-11-26 00:32:17 +0900

    jwtMiddleware set, req.user???

--- server/src/lib/token.js
+++ server/src/lib/token.js
@@ -12,4 +12,38 @@ function generateToken(payload) {
   )
 }
 
+function decodeToken(token) {
+  return new Promise(
+    (resolve, reject) => {
+      jwt.verify(token, jwtSecret, (err, decoded) => {
+        if (err) reject(err)
+        resolve(decoded)
+      })
+    }
+  )
+}
+
 exports.generateToken = generateToken
+
+exports.jwtMiddleware = async (req, res, next) => {
+  const token = req.cookie['access_token']
+  if (!token) return next()
+
+  try {
+    const decoded = await decodedToken(token)
+
+    if (Date.now() / 1000 - decoded.iat> 60 * 60 * 24) {
+      const { _id, profile } = decoded
+      const freshToken = await generateToken({ _id, profile }, 'account')
+      res.cookie('access_token', freshToken, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+      })
+    }
+    req.user(decoded)
+  } catch (e) {
+    req.user = null
+  }
+
+  return next()
+}
