09a4498c9698eb3ec5ee83e86dce1e96d5094912
kidow <wcgo2ling@hanmail.net>
2018-11-25 20:19:41 +0900

    books get set

--- server/src/api/books/books.controller.js
+++ server/src/api/books/books.controller.js
@@ -3,13 +3,37 @@ const Book = require('../../models/book')
 exports.list = async (req, res) => {
   let books
   try {
-    books = await Book.find().exec()
+    books = await Book.find()
+                      .sort({ _id: -1})
+                      .limit(3)
+                      .exec()
     res.json(books)
   } catch (e) {
     res.status(500)
     console.log(e)
   }
 };
+
+exports.get = async (req, res) => {
+  const { id } = req.params
+  let book
+  try {
+    book = await Book.findById(id).exec()
+    if (!book) {
+      res.status(404)
+      res.json({ message: 'book not found' })
+      return
+    }
+    res.json(book)
+  } catch (e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400
+      return
+    }
+    res.status(500)
+    console.log(e)
+  }
+}
 
 exports.create = async (req, res) => {
   const { title, authors, publishedDate, price, tags } = req.body
