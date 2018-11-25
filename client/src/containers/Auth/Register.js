e61dfa505fdae38d078a184b4fb44a5edd8060b6
kidow <wcgo2ling@hanmail.net>
2018-11-26 01:46:18 +0900

    containers login register set

---
+++ client/src/containers/Auth/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AuthContent from '../../components/Auth/AuthContent';
+
+class Register extends Component {
+  render() {
+    return (
+      <AuthContent title='회원가입'>
+
+      </AuthContent>
+    );
+  }
+}
+
+export default Register;
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AuthWrapper from '../components/Auth/AuthWrapper';
+import { Route } from 'react-router-dom'
+import Login from '../containers/Auth/Login';
+import Register from '../containers/Auth/Register';
 
 const AuthPage = () => {
   return (
     <AuthWrapper>
-      Hi
+      <Route path='/auth/login' component={Login}/>
+      <Route path='/auth/register' component={Register}/>
     </AuthWrapper>
   );
 };
