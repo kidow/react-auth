e5db8af0ec40df4e34d4efb336158424f9508c17
kidow <wcgo2ling@hanmail.net>
2018-11-26 03:04:28 +0900

    register input validation set

--- client/src/containers/Auth/Register.js
+++ client/src/containers/Auth/Register.js
@@ -6,12 +6,56 @@ import RightAlignedLink from 'components/Auth/RightAlignedLink'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as authActions from 'store/auth'
+import { isEmail, isLength, isAlphanumeric } from 'validator'
 
 class Register extends Component {
+  setError = message => {
+    const { AuthActions } = this.props
+    AuthActions.setError({
+      form: 'register',
+      message
+    })
+  }
+
+  validate = {
+    email: value => {
+      if (!isEmail(value)) {
+        this.setError('잘못된 이메일 형식입니다.')
+        return false
+      }
+      return true
+    },
+    username: value => {
+      if (!isAlphanumeric(value) || !isLength(value, { min: 4, max: 15 })) {
+        this.setError('아이디는 4~15 글자의 알파벳 혹은 숫자로 이뤄져야 합니다.')
+        return false
+      }
+      return true
+    },
+    password: value => {
+      if (!isLength(value, { min: 6 })) {
+        this.setError('비밀번호를 6자 이상 입력하세요.')
+        return false
+      }
+      this.setError(null); // 이메일과 아이디는 에러 null 처리를 중복확인 부분에서 하게 됩니다
+      return true
+    },
+    passwordConfirm: value => {
+      if (this.props.form.get('password') !== value) {
+        this.setError('비밀번호가 일치하지 않습니다.');
+        return false;
+      }
+      this.setError(null);
+      return true;
+    }
+  }
+
   handleChange = e => {
     const { AuthActions } = this.props
     const { name, value } = e.target
     AuthActions.changeInput({ name, value, form: 'register' })
+    const validation = this.validate[name](value)
+    if (name.indexOf('password') > -1 || !validation) return; // 비밀번호 검증이거나, 검증 실패하면 여기서 마침
   }
 
   componentWillUnmount() {
