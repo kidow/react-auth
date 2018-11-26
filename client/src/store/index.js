fa2ad4be978f2b91ea0ae9fe43bbda718b4de9ff
kidow <wcgo2ling@hanmail.net>
2018-11-26 14:18:07 +0900

    store user set

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import base from './base'
 import auth from './auth'
+import user from './user'
 import { combineReducers } from 'redux'
 import { penderReducer } from 'redux-pender'
 
 export default combineReducers({
   base,
   auth,
+  user,
   pender: penderReducer
 })
