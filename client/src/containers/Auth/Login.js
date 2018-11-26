31f5a6ce592b2e9cb064ce70c2fd9ede35e6720d
kidow <wcgo2ling@hanmail.net>
2018-11-26 15:02:14 +0900

    containers Login set

--- client/src/containers/Auth/Login.js
+++ client/src/containers/Auth/Login.js
@@ -3,15 +3,48 @@ import AuthContent from 'components/Auth/AuthContent';
 import InputWithLabel from 'components/Auth/InputWithLabel';
 import AuthButton from 'components/Auth/AuthButton'
 import RightAlignedLink from 'components/Auth/RightAlignedLink'
+import AuthError from 'components/Auth/AuthError'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as authActions from 'store/auth'
+import * as userActions from 'store/user'
+import storage from 'lib/storage'
 
 class Login extends Component {
   handleChange = e => {
     const { AuthActions } = this.props
     const { name, value } = e.target
-    AuthActions.changeInput({name, value, form: 'login'})
+    AuthActions.changeInput({
+      name, 
+      value, 
+      form: 'login'
+    })
+  }
+
+  setError = message => {
+    const { AuthActions } = this.props
+    AuthActions.setError({
+      form: 'login',
+      message
+    })
+    return false
+  }
+
+  handleLogin = async () => {
+    const { form, AuthActions, UserActions, history } = this.props
+    const { email, password } = form.toJS()
+
+    try {
+      await AuthActions.localLogin({email, password})
+      const loggedInfo = this.props.result.toJS()
+
+      UserActions.setLoggedInfo(loggedInfo)
+      history.push('/')
+      storage.set('loggedInfo', loggedInfo)
+    } catch (e) {
+      console.log(e)
+      this.setError('잘못된 계정 정보입니다.')
+    }
   }
 
   componentWillUnmount() {
@@ -20,7 +53,8 @@ class Login extends Component {
   }
   render() {
     const { email, password } = this.props.form.toJS()
-    const { handleChange } = this
+    const { handleChange, handleLogin } = this
+    const { error } = this.props
     return (
       <AuthContent title='로그인'>
         <InputWithLabel 
@@ -38,7 +72,8 @@ class Login extends Component {
           value={password}
           onChange={handleChange}
         />
-        <AuthButton>로그인</AuthButton>
+        {error && <AuthError>{error}</AuthError>}
+        <AuthButton onClick={handleLogin}>로그인</AuthButton>
         <RightAlignedLink to='/auth/register'>회원가입</RightAlignedLink>
       </AuthContent>
     );
@@ -47,9 +82,12 @@ class Login extends Component {
 
 export default connect(
   state => ({
-    form: state.auth.getIn(['login', 'form'])
+    form: state.auth.getIn(['login', 'form']),
+    error: state.auth.getIn(['login', 'error']),
+    result: state.auth.get('result')
   }),
   dispatch => ({
-    AuthActions: bindActionCreators(authActions, dispatch)
+    AuthActions: bindActionCreators(authActions, dispatch),
+    UserActions: bindActionCreators(userActions, dispatch)
   })
 )(Login);
