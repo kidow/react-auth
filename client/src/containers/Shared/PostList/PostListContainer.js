169f493fae8d8e28e16cc60045c184a5a1d5541c
kidow <wcgo2ling@hanmail.net>
2018-11-27 02:18:49 +0900

    PostListContainer redux connect

--- client/src/containers/Shared/PostList/PostListContainer.js
+++ client/src/containers/Shared/PostList/PostListContainer.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
-import PostList from 'components/Shared/PostList';
+import React, { Component } from 'react'
+import PostList from 'components/Shared/PostList'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as postsActions from 'store/posts'
 
 class PostListContainer extends Component {
+  load = async () => {
+    const { PostsActions } = this.props
+    PostsActions.lodaPost()
+  }
+
+  componentDidMount() {
+    this.load()
+  }
+  
   render() {
     return (
       <PostList />
-    );
+    )
   }
 }
 
-export default PostListContainer;
+export default connect(
+  state => ({
+    next: state.posts.get('next'),
+    data: state.posts.get('data')
+  }),
+  dispatch => ({
+    PostsActions: bindActionCreators(postsActions, dispatch)
+  })
+)(PostListContainer)
