b3507cf082a4313b608d65ad4f4ab05cf3bb2a69
kidow <wcgo2ling@hanmail.net>
2018-11-27 00:17:10 +0900

    writePost redux set

--- client/src/store/home.js
+++ client/src/store/home.js
@@ -1,18 +1,28 @@
-import { createAction, handleActions } from 'redux-actions';
-import { Map } from 'immutable';
+import { createAction, handleActions } from 'redux-actions'
+import { Map } from 'immutable'
+import { pender } from 'redux-pender'
+import * as api from 'lib/api/posts'
 
-const CHANGE_WRITE_POST_INPUT = 'home/CHANGE_WRITE_POST_INPUT';
+const CHANGE_WRITE_POST_INPUT = 'home/CHANGE_WRITE_POST_INPUT'
+const WRITE_POST = 'home/WRITE_POST'
 
-export const changeWritePostInput = createAction(CHANGE_WRITE_POST_INPUT);
+export const changeWritePostInput = createAction(CHANGE_WRITE_POST_INPUT)
+export const writePost = createAction(WRITE_POST, api.write)
 
 const initialState = Map({
   writePost: Map({
     value: ''
   })
-});
+})
 
 export default handleActions({
   [CHANGE_WRITE_POST_INPUT]: (state, action) => {
     return state.setIn(['writePost', 'value'], action.payload)
-  }
-}, initialState); 
+  },
+  ...pender({
+    type: WRITE_POST,
+    onPending: (state, action) => {
+      return state.setIn(['writePost', 'value'], '')
+    }
+  })
+}, initialState) 
