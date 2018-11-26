e6fc801d7db0707d6b0b0395b220cd80dfc87a19
kidow <wcgo2ling@hanmail.net>
2018-11-26 23:22:10 +0900

    writePost textarea set

--- client/src/components/Home/WritePost/WritePost.js
+++ client/src/components/Home/WritePost/WritePost.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './WritePost.scss'
+import TextArea from 'react-textarea-autosize'
 
-const WritePost = ({children}) => {
+const WritePost = ({onChange, value}) => {
   return (
     <div className='write-post'>
-      {children}
+      <TextArea 
+        className='textarea'
+        minRows={3}
+        maxRows={10}
+        placeholder={`의식의 흐름대로 당신의 생각을 적어보세요.\n5초이상 아무것도 입력하지 않으면 자동으로 포스팅됩니다.`}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
