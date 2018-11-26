3b54abf9f8364e6d3bb79d0896fb6abdab35e17e
kidow <wcgo2ling@hanmail.net>
2018-11-26 15:17:04 +0900

    logout set

--- client/src/containers/Base/HeaderContainer.js
+++ client/src/containers/Base/HeaderContainer.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Header from 'components/Base/Header';
 import LoginButton from 'components/Base/LoginButton'
+import * as userActions from 'store/user'
+import { bindActionCreators } from 'redux'
+import storage from 'lib/storage'
 
 class HeaderContainer extends Component {
+  handleLogout = async () => {
+    const { UserActions } = this.props
+    try {
+      await UserActions.logout()
+    } catch (e) {
+      console.log(e)
+    }
+    storage.remove('loggedInfo')
+    window.location.href = '/'
+  }
+
   render() {
+    const { handleLogout } = this
+    const { user } = this.props
     return (
       <Header>
-        <LoginButton />
+        {user.get('logged') ? (
+          <div>
+            {user.getIn(['loggedInfo', 'username'])}
+            <div onClick={handleLogout}>로그아웃</div>
+          </div>
+        ) : (
+          <LoginButton />
+        )}
       </Header>
     );
   }
@@ -15,9 +38,9 @@ class HeaderContainer extends Component {
 
 export default connect(
   state => ({
-    
+    user: state.user
   }),
   dispatch => ({
-
+    UserActions: bindActionCreators(userActions, dispatch)
   })
 )(HeaderContainer);
