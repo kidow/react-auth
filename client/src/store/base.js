c23c6a3d73f6299f47233bfa806ae7a8d42adaee
kidow <wcgo2ling@hanmail.net>
2018-11-26 20:13:16 +0900

    user actions set

--- client/src/store/base.js
+++ client/src/store/base.js
@@ -2,17 +2,25 @@ import { Map } from 'immutable'
 import { handleActions, createAction } from 'redux-actions'
 
 const SET_HEADER_VISIBILITY = 'base/SET_HEADER_VISIBILITY'
+const SET_USER_MENU_VISIBILITY = 'base/SET_USER_MENU_VISIBILITY'
 
 export const setHeaderVisibility = createAction(SET_HEADER_VISIBILITY)
+export const setUserMenuVisibility = createAction(SET_USER_MENU_VISIBILITY)
 
 const initialState = Map({
   header: Map({
     visible: true
+  }),
+  userMenu: Map({
+    visible: false
   })
 })
 
 export default handleActions({
   [SET_HEADER_VISIBILITY]: (state, action) => {
     return state.setIn(['header', 'visible'], action.payload)
+  },
+  [SET_USER_MENU_VISIBILITY]: (state, action) => {
+    return state.setIn(['userMenu', 'visible'], action.payload)
   }
 }, initialState)
