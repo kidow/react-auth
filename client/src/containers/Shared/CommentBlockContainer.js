d66edb8044e73af1e0cb92d5209abd1195e7745c
kidow <wcgo2ling@hanmail.net>
2018-11-28 20:14:38 +0900

    comment input onchange set

--- client/src/containers/Shared/CommentBlockContainer.js
+++ client/src/containers/Shared/CommentBlockContainer.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import { CommentBlock } from 'components/Shared';
 import { connect } from 'react-redux'
+import * as postsActions from 'store/posts'
+import { bindActionCreators } from 'redux'
 
 class CommentBlockContainer extends Component {
+  handleChange = e => {
+    const { value } = e.target
+    const { PostsActions, post } = this.props
+    PostsActions.changeCommentInput({
+      postId: post.get('_id'),
+      value
+    })
+  }
+
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      console.log('wa!')
+    }
+  }
   render() {
     const { status } = this.props
     const { visible, value } = status ? status.toJS() : {}
+    const { handleChange, handleKeyPress } = this
+
+    if (!visible) return null
+    
     return (
-      <CommentBlock value={value}/>
+      <CommentBlock 
+        value={value}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
     );
   }
 }
@@ -15,5 +39,8 @@ class CommentBlockContainer extends Component {
 export default connect(
   (state, ownProps) => ({
     status: state.posts.getIn(['comments', ownProps.post.get('_id')])
+  }),
+  dispatch => ({
+    PostsActions: bindActionCreators(postsActions, dispatch)
   })
 )(CommentBlockContainer);
