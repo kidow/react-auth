62b983e753e149ac424d38f9cc1d324eb8d02221
kidow <wcgo2ling@hanmail.net>
2018-11-28 19:51:22 +0900

    comment redux set

--- client/src/store/posts.js
+++ client/src/store/posts.js
@@ -7,19 +7,33 @@ const LOAD_POST = 'posts/LOAD_POST'
 const PREFETCH_POST = 'posts/PREFETCH_POST'
 const SHOW_PREFETCHED_POST = 'posts/SHOW_PREFETCHED_POST'
 const RECEIVE_NEW_POST = 'posts/RECEIVE_NEW_POST'
+
 const LIKE_POST = 'posts/LIKE_POST'
-const DISLIKE_POST = 'posts/DISLIKE_POST' 
+const DISLIKE_POST = 'posts/DISLIKE_POST'
+
+const TOGGLE_COMMENT = 'posts/TOGGLE_COMMENT'
+const CHANGE_COMMENT_INPUT = 'posts/CHANGE_COMMENT_INPUT'
 
 export const loadPost = createAction(LOAD_POST, api.list)
 export const prefetchPost = createAction(PREFETCH_POST, api.next)
 export const showPrefetchedPost = createAction(SHOW_PREFETCHED_POST)
+
 export const likePost = createAction(LIKE_POST, api.like, payload => payload)
 export const dislikePost = createAction(DISLIKE_POST, api.dislike, payload => payload)
+
+export const toggleComment = createAction(TOGGLE_COMMENT)
+export const changeCommentInput = createAction(CHANGE_COMMENT_INPUT)
 
 const initialState = Map({
   next: '',
   data: List(),
-  nextData: List()
+  nextData: List(),
+  comments: Map({
+    _postId: Map({
+      visible: false,
+      value: ''
+    })
+  })
 })
 
 export default handleActions({
@@ -78,5 +92,19 @@ export default handleActions({
       const index = state.get('data').findIndex(post => post.get('_id') === action.meta)
       return state.setIn(['data', index, 'likesCount'], likesCount)
     }
-  })
+  }),
+  [TOGGLE_COMMENT]: (state, action) => {
+    const comment = state.getIn(['comments', action.payload])
+    if (comment) {
+      return state.updateIn(['comments', action.payload], comment => comment.set('visible', !comment.get('visible')))
+    }
+    return state.setIn(['comments', action.payload], Map({
+      visible: true,
+      value: ''
+    }))
+  },
+  [CHANGE_COMMENT_INPUT]: (state, action) => {
+    const { postId, value } = action.payload
+    return state.setIn(['comments', postId, 'value'], value)
+  }
 }, initialState) 
