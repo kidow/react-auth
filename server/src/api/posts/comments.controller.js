46989a795630238c29d782008f0a8f5af8c7c1d9
kidow <wcgo2ling@hanmail.net>
2018-11-28 19:35:15 +0900

    write comments api set

---
+++ server/src/api/posts/comments.controller.js
@@ -0,0 +1,52 @@
+const Joi = require('joi')
+const Post = require('../../models/post')
+const { Types: { ObjectId } } = require('mongoose')
+
+exports.comment = async (req, res) => {
+  const { user } = req.user
+  if (!user) {
+    res.status(403)
+    return
+  }
+
+  const schema = Joi.object().keys({
+    text: Joi.string().min(1).max(100).required()
+  })
+
+  const result = Joi.validate(req.body, schema)
+  if (result.error) {
+    res.status(400)
+    return
+  }
+
+  const { username } = user.profile
+  const { text } = req.body
+  const { postId } = req.params
+
+  if (!ObjectId.isValid(postId)) {
+    res.status(40)
+    return
+  }
+
+  let post = null
+  try {
+    post = await Post.findById(postId)
+  } catch (e) {
+    console.log(e)
+    res.status(500)
+  }
+
+  if (!post) {
+    res.status(404)
+    return
+  }
+
+  try {
+    await Post.writeComment({username, text})
+  } catch (e) {
+    console.log(e)
+    res.status(500)
+  }
+
+  res.json(post.comments)
+}
--- server/src/api/posts/index.js
+++ server/src/api/posts/index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const posts = express.Router()
 const postsCtrl = require('./posts.controller')
 const likesCtrl = require('./likes.controller')
+const commentCtrl = require('./comments.controller')
 
 posts.post('/', postsCtrl.write)
 posts.get('/', postsCtrl.list)
 posts.post('/:postId/likes', likesCtrl.like)
 posts.delete('/:postId/likes', likesCtrl.dislike)
+posts.post('/:postId/comments', commentCtrl.comment)
 
 module.exports = posts
