d68d59c5b79312e1f86c6423404e5aa697c178ca
kidow <wcgo2ling@hanmail.net>
2018-11-28 17:25:29 +0900

    create post method like, unlike

--- server/src/models/post.js
+++ server/src/models/post.js
@@ -33,4 +33,24 @@ Post.statics.list = function({cursor, username, self}) {
   return this.find(query).sort({ _id: -1 }).limit(20).exec()
 }
 
+Post.statics.like = function({_id, username}) {
+  return this.findByIdAndUpdate(_id, {
+    $inc: { likesCount: 1 },
+    $push: { likes: username }
+  }, {
+    new: true,
+    select: 'likesCount'
+  }).exec()
+}
+
+Post.statics.unlike = function({_id, username}) {
+  return this.findByIdAndUpdate(_id, {
+    $inc: { likesCount: -1 },
+    $pull: { likes: username }
+  }, {
+    new: true,
+    select: 'likesCount'
+  })
+}
+
 module.exports = mongoose.model('Post', Post)
